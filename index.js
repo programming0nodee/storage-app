a840052825f80e2dff53d5b37c3a0cc721907c53
programming0nodee <programming0nodee+github@gmail.com>
2025-03-30 13:01:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,13 +63,12 @@ app.get('/', (_, res) => {
 });
 
 app.get('/healthcheck', (req, res) => {
-    let date = new Date().toUTCString();
-    res.setHeader('Content-Type', 'text/txt;charset=utf-8')
-       .setHeader('Date-Request', req.headers['date-request'])
-       .setHeader('Date-Response', date)
-       .setHeader('Cache-Control', 'no-cache')
-       .status(200)
-       .send('OK');
+  res.set({
+    'Content-Type': 'text/plain;charset=utf-8',
+    'Date-Request': req.headers['date-request'],
+    'Date-Response': date,
+    'Cache-Control': 'no-cache'
+  }).status(200).send('OK');
 });
 
 app.get('/gen:code', (req, res) => {
